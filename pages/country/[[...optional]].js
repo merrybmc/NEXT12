d4b7107af31a57c36062fb697bc1c1c7c73de73f
Byeong Min Cho <merrybmc@gmail.com>
2024-04-07 00:46:39 +0900

    페이지 이동하기

--- pages/country/[[...optional]].js
+++
@@ -1,8 +0,0 @@
-import { useRouter } from 'next/router';
-import React from 'react';
-
-export default function Optional() {
-  const router = useRouter();
-  const { optional } = router.query;
-  return <div>optional {optional}</div>;
-}
